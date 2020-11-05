c65747d7b1cc790fee6ab88af08f0e8053462910
feliciavonbraun <feliciavonbraun@hotmail.com>
2020-11-05 21:00:24 +0100

    deleted drafts and added alot

---
+++ script.js
@@ -0,0 +1,62 @@
+const header = document.querySelector('header');
+// const main = document.querySelector('main');
+const menuIcon = document.getElementById('menu');
+
+menuIcon.onclick = function() {
+    if (header.style.height == '100%') {
+        header.style.height = null; 
+    } else {
+        header.style.height = '100%';
+    }
+}
+
+
+
+
+
+
+
+window.onload = changeActiveImage;
+// window.addEventListener('load', main);
+
+
+/** Start the animation with a set interval */
+function startSlideshowAnimation() {
+    setInterval(changeActiveImage, 2000);
+}
+
+/** Presents the next image by checking which image is currently presented */
+function changeActiveImage() {
+    const images = document.getElementById('slideshow-container');
+
+    for (let i = 0; i < images.length; i++) {
+        
+        if (images[i].classList.contains('show')) {
+            images[i].classList.remove('show');
+
+            // Make sure that we go back to index 0 when at the end of array.
+            let nextIndex = (i + 1) % images.length;
+            
+            // Alternatively use an if statement to reset the index to 0.
+            // if (nextIndex >= images.length) {
+            //     nextIndex = 0;
+            // }
+
+            // Modulus rocks!
+            // 0 / 4 = 0
+            // 1 / 4 = 1  
+            // 2 / 4 = 2
+            // 3 / 4 = 3
+            // 4 / 4 = 0
+            // 5 / 4 = 1
+            // 6 / 4 = 2
+            // 7 / 4 = 3
+            // 8 / 4 = 0
+            // 9 / 4 = 1
+
+            images[nextIndex].classList.add('show');
+            break;
+        }
+        
+    }
+}
