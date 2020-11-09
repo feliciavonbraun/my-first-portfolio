cf480539b558c9c1bd755d1e6de5bd35a123a8b8
feliciavonbraun <feliciavonbraun@hotmail.com>
2020-11-09 23:56:52 +0100

    all links fixed and more

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const menuIcon1 = document.getElementById('menu1');
 menuIcon1.onclick = function() {
     if (burgerNav.style.height == '100%') {
         burgerNav.style.height = null;
+        burgerNav.style.background = null;    
     } else {
         burgerNav.style.height = '100%';
+        burgerNav.style.background = "rgb(134, 91, 121)";    
     }
 }
 
