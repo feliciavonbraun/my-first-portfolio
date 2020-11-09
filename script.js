964e4fbeb8d42b669c64559f5a0bc516cddcef3f
feliciavonbraun <feliciavonbraun@hotmail.com>
2020-11-09 13:37:40 +0100

    started on mobile query and more

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Burger menu2 (on contact page)
+// Burger menu2 (on contact page) WORKING
 const header = document.querySelector('header');
-const menuIcon = document.getElementById('menu2');
+const menuIcon2 = document.getElementById('menu2');
 
-menuIcon.onclick = function() {
+menuIcon2.onclick = function() {
     if (header.style.height == '40%') {
         header.style.height = null; 
     } else {
@@ -12,17 +12,19 @@ menuIcon.onclick = function() {
 
 
 //Burger menu1 (on homepage)
-const main = document.getElementsByClassName('burgerNav');
+const burgerNav = document.getElementsByClassName('burgerNav');
 const menuIcon1 = document.getElementById('menu1')
 
 // burger nav ska ökas inte header 
 
 menuIcon1.onclick = function() {
     if (burgerNav.style.height == '100%') {
-         burgerNav.style.height = null;
-     } else {
-         burgerNav.style.height = '100%';
-     }
+        burgerNav.style.height = null;
+    } else {
+        burgerNav.style.height = '100%';
+    }
+
+    console.log('test')
 }
 
 
@@ -32,47 +34,47 @@ menuIcon1.onclick = function() {
 
 
 // window.onload = changeActiveImage;
-window.addEventListener('load', main);
+// window.addEventListener('load', main);
 
 
 /** Start the animation with a set interval */
-function startSlideshowAnimation() {
-    setInterval(changeActiveImage, 2000);
-}
+// function startSlideshowAnimation() {
+//     setInterval(changeActiveImage, 2000);
+// }
 
-/** Presents the next image by checking which image is currently presented */
-function changeActiveImage() {
-    const images = document.getElementById('slideshow-container');
+// /** Presents the next image by checking which image is currently presented */
+// function changeActiveImage() {
+//     const images = document.getElementById('slideshow-container');
 
-    for (let i = 0; i < images.length; i++) {
+//     for (let i = 0; i < images.length; i++) {
         
-        if (images[i].classList.contains('show')) {
-            images[i].classList.remove('show');
+//         if (images[i].classList.contains('show')) {
+//             images[i].classList.remove('show');
 
-            // Make sure that we go back to index 0 when at the end of array.
-            let nextIndex = (i + 1) % images.length;
+//             // Make sure that we go back to index 0 when at the end of array.
+//             let nextIndex = (i + 1) % images.length;
             
-            // Alternatively use an if statement to reset the index to 0.
-            // if (nextIndex >= images.length) {
-            //     nextIndex = 0;
-            // }
+//             // Alternatively use an if statement to reset the index to 0.
+//             // if (nextIndex >= images.length) {
+//             //     nextIndex = 0;
+//             // }
 
-            // Modulus rocks!
-            // 0 / 4 = 0
-            // 1 / 4 = 1  
-            // 2 / 4 = 2
-            // 3 / 4 = 3
-            // 4 / 4 = 0
-            // 5 / 4 = 1
-            // 6 / 4 = 2
-            // 7 / 4 = 3
-            // 8 / 4 = 0
-            // 9 / 4 = 1
+//             // Modulus rocks!
+//             // 0 / 4 = 0
+//             // 1 / 4 = 1  
+//             // 2 / 4 = 2
+//             // 3 / 4 = 3
+//             // 4 / 4 = 0
+//             // 5 / 4 = 1
+//             // 6 / 4 = 2
+//             // 7 / 4 = 3
+//             // 8 / 4 = 0
+//             // 9 / 4 = 1
 
-            images[nextIndex].classList.add('show');
-            break;
-        }
+//             images[nextIndex].classList.add('show');
+//             break;
+//         }
         
-    }
-}
+//     }
+// }
 
