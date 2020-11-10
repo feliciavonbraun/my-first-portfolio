c0331055e82e1480914f4b31badd0b6f25dc6e17
feliciavonbraun <feliciavonbraun@hotmail.com>
2020-11-10 21:35:11 +0100

    mostly about section fixed

--- contact.js
+++ contact.js
@@ -3,11 +3,11 @@ const header = document.querySelector('header');
 const menuIcon2 = document.getElementById('menu2');
 
 menuIcon2.onclick = function() {
-    if (header.style.height == '30%') {
+    if (header.style.height == '40%') {
            header.style.height = null; 
            header.style.background = null;
     } else {
-           header.style.height = '30%';
+           header.style.height = '40%';
            header.style.background = 'rgb(134, 91, 121)';
     }
 }
