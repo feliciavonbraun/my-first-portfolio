32e339d41ddf0785a73969bdcfa2d6cf00264d91
feliciavonbraun <feliciavonbraun@hotmail.com>
2020-11-13 18:26:17 +0100

    added pictures and responivitet

--- javascript/contact.js
+++ javascript/contact.js
@@ -2,12 +2,13 @@
 const header = document.querySelector('header');
 const menuIcon2 = document.getElementById('menu2');
 
+/** @function shows and hides menu onclick */
 menuIcon2.onclick = function() {
-    if (header.style.height == '40%') {
+    if (header.style.height == '60%') {
            header.style.height = null; 
            header.style.background = null;
     } else {
-           header.style.height = '40%';
+           header.style.height = '60%';
            header.style.background = 'rgb(134, 91, 121)';
     }
 }
--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,9 @@
-//Burger menu1 (on homepage)
+// Burger menu on homepage
 const burgerNav = document.getElementsByClassName('burgerNav')[0];
 const menuIcon1 = document.getElementById('menu1');
 
+
+/** @function shows and hides menu onclick */
 menuIcon1.onclick = function() {
     if (burgerNav.style.height == '100%') {
         burgerNav.style.height = null;
