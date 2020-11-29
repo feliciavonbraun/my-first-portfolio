ef56fd523928cd1586d7a7ee57f980fbfc543c37
feliciavonbraun <feliciavonbraun@hotmail.com>
2020-11-29 22:53:33 +0100

    cleaned up code

--- javascript/contact.js
+++ javascript/contact.js
@@ -4,11 +4,11 @@ const menuIcon2 = document.getElementById('menu2');
 
 /** @function shows and hides menu onclick */
 menuIcon2.onclick = function() {
-    if (header.style.height == '60%') {
+    if (header.style.height == '70%') {
            header.style.height = null; 
            header.style.background = null;
     } else {
-           header.style.height = '60%';
+           header.style.height = '70%';
            header.style.background = 'rgb(134, 91, 121)';
     }
 }
